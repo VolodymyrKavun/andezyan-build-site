e2c7086bb907d6238f9081b6fed4e934c6435a31
Webevery <webforevery@gmail.com>
2023-07-14 17:38:13 +0300

    add_data+folder_designCategory

---
+++ src/data/designCategory.data.js
@@ -0,0 +1,149 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const designCategory = [
+  // sertificate
+  {
+    id: uuidv4(),
+    src: '/certificates/certificate_lobas.jpg',
+    alt: 'Certificate lobas',
+    category: 'sertificate',
+  },
+  {
+    id: uuidv4(),
+    src: '/certificates/qualification_lobas.jpg',
+    alt: 'Qualification lobas',
+    category: 'sertificate',
+  },
+
+  // documentation
+
+  {
+    id: uuidv4(),
+    src: '/documentation/1_page.webp',
+    alt: '1_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/2_page.webp',
+    alt: '2_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/3_page.webp',
+    alt: '3_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/4_page.webp',
+    alt: '4_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/5_page.webp',
+    alt: '5_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/6_page.webp',
+    alt: '6_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/7_page.webp',
+    alt: '7_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/8_page.webp',
+    alt: '8_page',
+    category: 'documentation',
+  },
+  {
+    id: uuidv4(),
+    src: '/documentation/9_page.webp',
+    alt: '9_page',
+    category: 'documentation',
+  },
+
+  //visualization
+
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_dark_grey_front.webp',
+    alt: 'angar_dark_grey_front',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_grey_front.webp',
+    alt: 'angar_grey_front',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_grey_front_dark_gate.webp',
+    alt: 'angar_grey_front_dark_gate',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_grey_front_sceleton.webp',
+    alt: 'angar_grey_front_sceleton',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_grey_roof_inside_sceleton.webp',
+    alt: 'angar_grey_roof_inside_sceleton',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/angar_grey_roof_sceleton.webp',
+    alt: 'angar_grey_roof_sceleton',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/metal_stairs.webp',
+    alt: 'metal_stairs',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/two_storey_office_back.webp',
+    alt: 'two_storey_office_back',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/two_storey_office_front.webp',
+    alt: 'two_storey_office_front',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/two_storey_office_front_finish.webp',
+    alt: 'two_storey_office_front_finish',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/two_storey_office_front_right.webp',
+    alt: 'two_storey_office_front_right',
+    category: 'visualization',
+  },
+  {
+    id: uuidv4(),
+    src: '/visualization/two_storey_office_front_with_green.webp',
+    alt: 'two_storey_office_front_with_green',
+    category: 'visualization',
+  },
+];
