f57a6f69ac046a68d84dd29cde18b7fdbe6f00be
Maksimchik36 <Peltek1985@gmail.com>
2023-07-16 16:32:18 +0300

    created Slider with Lightbox,getDataByParam and createPath in utils, changed data

--- src/components/screens/Gallery/Gallery.jsx
+++ src/components/screens/Gallery/Gallery.jsx
@@ -11,13 +11,11 @@ const Gallery = () => {
       <Link key={item.id} href={item.url} className={styles.item}>
         <div className={styles.imgContainer}>
           <Image
+            className={styles.img}
             src={item.img}
             alt={item.title}
-            // width={328}
-            // height={320}
             width={380}
             height={380}
-            className={styles.img}
           />
           <h1 className={styles.title}>{item.title}</h1>
         </div>
