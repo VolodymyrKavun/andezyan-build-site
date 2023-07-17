667bdd928b7248cbb67cf5f1dec9659529e56c93
Webevery <webforevery@gmail.com>
2023-07-17 18:42:17 +0300

    add_video_page

--- src/components/screens/Video/Video.jsx
+++ src/components/screens/Video/Video.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styles from './Video.module.css';
+import { videosData } from '@/data/videos.data';
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <>
+      <section className={styles.containerVideo}>
+        <ul className={styles.listVideo}>
+          {videosData.map(el => {
+            return (
+              <li className={styles.itemVideo} key={el.id}>
+                <iframe
+                  className={styles.imgVideo}
+                  width="100%"
+                  height="100%"
+                  src={el.src}
+                  title={el.title}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                />
+                <p className={styles.titleVideo}>{el.description}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Video;
