94d3998b705734e0e9e0f1a62c9e9e2c5d575c6e
Webevery <webforevery@gmail.com>
2023-07-21 17:51:46 +0300

    add_btn_back_to_top+neon_footer

--- src/components/ui/BackToTop/BackToTop.jsx
+++ src/components/ui/BackToTop/BackToTop.jsx
@@ -1,7 +1,53 @@
+'use client';
 import React from 'react';
+import styles from './BackToTop.module.css';
+import Image from 'next/image';
 
 const BackToTop = () => {
-  return <div>BackToTop</div>;
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    let backToTop = document.getElementById('backToTop');
+    if (!backToTop) return;
+    if (
+      document.body.scrollTop >= 200 ||
+      document.documentElement.scrollTop >= 200
+    ) {
+      // backToTop.style.display = 'block';
+      backToTop.className = styles.backToTop__visible;
+    } else {
+      // backToTop.style.display = 'none';
+      backToTop.className = styles.backToTop;
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0;
+  }
+
+  return (
+    <>
+      <button
+        id="backToTop"
+        type="button"
+        onClick={topFunction}
+        className={`${styles.backToTop} ${scrollFunction()}`}
+        aria-label="backToTop"
+      >
+        <div className={styles.iconBackWrapper}>
+          <Image
+            src="/sprite/icon_arrow-down-circle.svg"
+            alt="Кнопка до гори"
+            fill={true}
+            className={styles.backToTopIcon}
+          />
+        </div>
+      </button>
+    </>
+  );
 };
 
 export default BackToTop;
