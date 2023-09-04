baa4bb797f7e181a64372cdb8fb4b1db18ee96c6
Webevery <webforevery@gmail.com>
2023-09-04 15:25:21 +0300

    add: android+change_route_favicon

--- src/components/screens/Gallery/Gallery.jsx
+++ src/components/screens/Gallery/Gallery.jsx
@@ -1,34 +1,36 @@
-"use client";
+'use client';
 import React from 'react';
 import styles from './Gallery.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { imageGroupsData } from '@/data/imageGroups.data';
 
-
 const Gallery = () => {
-  return <section className={styles.container}>
-    <h1 className={styles.titleHidden}>Галерея будівельної компанії Андезян</h1>
-    <ul className={styles.list}>
-      {imageGroupsData.map(item => (
-        <li key={item.id}>
-          <Link href={item.url}>
-            <div className={styles.imgContainer}>
-              <Image
-                className={styles.img}
-                src={item.img}
-                alt={item.title}
-                width={380}
-                height={380}
-              />
-              <h2 className={styles.title}>{item.title}</h2>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </section>;
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.titleHidden}>
+        Галерея будівельної компанії Андезян
+      </h1>
+      <ul className={styles.list}>
+        {imageGroupsData.map(item => (
+          <li key={item.id}>
+            <Link href={item.url}>
+              <div className={styles.imgContainer}>
+                <Image
+                  className={styles.img}
+                  src={item.img}
+                  alt={item.title}
+                  width={380}
+                  height={380}
+                />
+                <h2 className={styles.title}>{item.title}</h2>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
-
 
 export default Gallery;
