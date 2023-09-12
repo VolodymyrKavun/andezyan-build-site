02d652b9dc42485f94e36d2f5268e3d3d06dc22b
Webevery <webforevery@gmail.com>
2023-09-12 18:23:30 +0300

    add: aria-label and li to ul

--- src/components/share/SocLinksButton/SocLinksButton.jsx
+++ src/components/share/SocLinksButton/SocLinksButton.jsx
@@ -2,34 +2,42 @@ import styles from './SocLinksButton.module.css';
 import { socialData } from '@/data/social.data';
 import { createArrOfNeededObjProperties } from '@/utils/createArrOfNeededObjProperties';
 
-
 const SocLinksButton = () => {
-
-  const neededArr = ["phone", "email", "telegram", "viber", "physicalAddress"];
+  const neededArr = ['phone', 'email', 'telegram', 'viber', 'physicalAddress'];
 
   const neededData = createArrOfNeededObjProperties(socialData, neededArr);
-
 
   return (
     <div className={styles.container}>
       <div className={styles.circleWrapper}>
         <svg className={styles.circle}>
-          <use href="/sprite.svg#icon_message_circle" className={styles.iconCircle}></use>
+          <use
+            href="/sprite.svg#icon_message_circle"
+            className={styles.iconCircle}
+          ></use>
         </svg>
       </div>
 
       <ul className={styles.socialList}>
         {neededData.map((item, index) => (
-          <a key={index} href={`${item.path}`} className={styles.socLink}>
-            <svg className={styles.socSvg}>
-              <use href={item.icon} className={`${styles.socIcon} ${styles[item.name]}`}  ></use>
-            </svg>
-          </a>
+          <li key={index}>
+            <a
+              href={`${item.path}`}
+              className={styles.socLink}
+              aria-label={item.name}
+            >
+              <svg className={styles.socSvg}>
+                <use
+                  href={item.icon}
+                  className={`${styles.socIcon} ${styles[item.name]}`}
+                ></use>
+              </svg>
+            </a>
+          </li>
         ))}
       </ul>
     </div>
   );
 };
-
 
 export default SocLinksButton;
