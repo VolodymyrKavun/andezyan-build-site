1ff200f4923601607b28083c4d65e1fbbbc7f617
Webevery <webforevery@gmail.com>
2023-09-13 17:58:29 +0300

    add: links in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -79,30 +79,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="uk">
       <Head>
-        {/* <link
+        <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/favicons/apple-touch-icon.png"
           type="image/png"
-        /> */}
-        {/* <link
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="16x16"
           href="/favicons/favicon-16x16.png"
-        /> */}
-        {/* <link
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="/favicons/favicon-32x32.png"
-        /> */}
-        {/* <link
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="48x48"
           href="/favicons/favicon-48x48.png"
-        /> */}
+        />
 
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
