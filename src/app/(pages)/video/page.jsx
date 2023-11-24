d59fdf5b9622887523d99777b214051789ae56c8
VolodymirKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2023-11-24 22:44:36 +0200

    add: last_exper_03

--- src/app/(pages)/video/page.jsx
+++ src/app/(pages)/video/page.jsx
@@ -39,6 +39,47 @@ const VideoPage = () => {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
+      <div
+        itemscope
+        itemtype="https://schema.org/Organization"
+        style={{ display: 'none' }}
+      >
+        <meta itemprop="name" content="Будівельна компанія Андезян" />
+        <link itemprop="url" href="https://andezyan-build-site.vercel.app/" />
+        <link
+          itemprop="logo"
+          href="https://andezyan-build-site.vercel.app/favicon.ico"
+        />
+        <meta
+          itemprop="description"
+          content="Ангар, склад, навіс, металоконструкції, реконструкція, Проектування будівельних споруд, Моделювання, Візуалізація та Розробка документації."
+        />
+        <meta
+          itemprop="keywords"
+          content="Будівельна компанія Андезян, Будівництво ангарів, Ангар, Склад"
+        />
+        <div
+          itemprop="address"
+          itemscope
+          itemtype="https://schema.org/PostalAddress"
+        >
+          <meta itemprop="addressLocality" content="Біла Церква, Україна" />
+          <meta itemprop="postalCode" content="09100" />
+          <meta itemprop="streetAddress" content="вул. Київська, 96" />
+        </div>
+        <div
+          itemprop="ContactPoint"
+          itemscope
+          itemtype="https://schema.org/ContactPoint"
+        >
+          <meta itemprop="email" content="aoandezyan@gmail.com" />
+          <meta itemprop="email" content="lotos0712@ukr.net" />
+          <meta itemprop="telephone" content="+380675909988" />
+          <meta itemprop="telephone" content="+380674630606" />
+          <meta itemprop="telephone" content="+380674630606" />
+          <meta itemprop="telephone" content="+380456051228" />
+        </div>
+      </div>
       <Video />
     </>
   );
